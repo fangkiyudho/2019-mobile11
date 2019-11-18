ae98193e471e0c3b78b6844b6803658ecbd209e7
fangkiyudho <fangkiyudho@gmail.com>
2019-11-18 20:28:41 +0700

    Setting fragment

---
+++ SettingsExample/app/src/main/java/id/ac/polinema/settingsexample/fragments/SettingsFragment.java
@@ -0,0 +1,36 @@
+package id.ac.polinema.settingsexample.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.preference.PreferenceFragmentCompat;
+import id.ac.polinema.settingsexample.R;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
